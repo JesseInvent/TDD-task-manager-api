4f69d90f16eb67edbf639b9b0e4cfabb1a6f3926
Jesse Ojih <jcinvent05@gmail.com>
2021-03-22 22:59:13 +0100

    Added JWTException handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,10 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -34,8 +36,10 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (Throwable $e) {
+            if ($e instanceof JWTException) {
+                return response()->json(['Token not provided'], Response::HTTP_BAD_REQUEST);
+            }
         });
     }
 }
